1a10055a8ae3dcdcea98cec11caeb28f4891125e
kushnotfromagra <kushagradangi10@gmail.com>
2025-01-15 17:30:45 +0530

    files added

---
+++ index.js
@@ -0,0 +1,27 @@
+const lengthDisplay=document.querySelector("[data-lengthNumber]");
+const inputSlider=document.querySelector("[data-lenngthSlider]");
+const indicator= document.querySelector("[data-Indicator]");
+
+
+defaultPasswordLength=10;
+
+handleSlider();
+
+function handleSlider(){
+    lengthDisplay.innerText= defaultPasswordLength;
+    inputSlider.value= defaultPasswordLength;
+
+}
+
+function setIndicato(color){
+    indicator.style.backgroundColor= color;
+}
+
+function getRandomInteger(min, max){
+    return Math.floor(Math.random()*(max-min))+min;
+}
+
+
+
+
+
